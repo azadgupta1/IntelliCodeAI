25068f23d1404e7526609f7f665b31adfb8eae3b
azadgupta1 <azad236820@gmail.com>
2025-01-03 11:30:27 +0530

    Completed upload files routes and tested it

---
+++ backend/uploads/1735883503164-104900832-even_odd.py
@@ -0,0 +1,13 @@
+def even_check(num):
+    if(num%2 == 0):
+        print("The number "+str(num)+" is even.")
+    else:
+        print("The number "+str(num)+" is odd.")
+    
+
+
+
+
+num = int(input("Enter the number: "))
+
+even_check(num)
