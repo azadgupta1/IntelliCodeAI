f3e38b6a4d9a786378c6b0d4852a9b799bbd9c2b
azadgupta1 <azad236820@gmail.com>
2025-02-06 17:35:35 +0530

    Webhooks

--- backend/src/routes/githubRoutes.js
+++ backend/src/routes/githubRoutes.js
@@ -5,7 +5,6 @@ import { authenticate } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Route to fetch and save user repositories
 router.get("/repos", authenticate, fetchUserRepos);
 
 router.get("/repos/:owner/:repo/commits/:commitSha", authenticate, fetchCommitDetails);
