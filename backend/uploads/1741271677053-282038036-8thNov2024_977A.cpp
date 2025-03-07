6e4033746138eba0ebebafa451a60d3857177941
azadgupta1 <azad236820@gmail.com>
2025-03-07 19:20:51 +0530

    API's done for analysis history

---
+++ backend/uploads/1741271677053-282038036-8thNov2024_977A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int k;
+
+    cin>> num >> k;
+
+    for(int i = 0; i<k; i++){
+        if(num % 10 == 0){
+            num = num/10;
+        }
+        else{
+            num = num - 1;
+        }
+    }
+
+    cout<<num;
+}
